44ca682f174b672e748ebb8268e65711a5e02981
Denny Wibowo <indocoffee@gmail.com>
2025-03-08 11:10:34 -0500

    Do insert cards, manually for testing

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,5 +1,45 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://root:password@localhost:27017/";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db("ccDb");
+    console.log("Database connected ", database);
+    const cards = database.collection("cards");
+    await cards.deleteMany();
+
+    const visaCard = {
+      name: "Visa",
+      image: "assets/visa.jpg",
+      features: ["3% cash return", "Milage points"],
+      rules: ["$90 annual fee"],
+    };
+
+    const amexCard = {
+      name: "American Express",
+      image: "assets/amex.jpg",
+      features: ["2% cash back", "Free rental insurance"],
+      rules: [],
+    };
+
+    const result = await cards.insertMany([visaCard, amexCard]);
+    console.log(`Document/s inserted: ${result}`);
+
+    const query = { name: "Visa" };
+    const card = await cards.findOne(query);
+    console.log("Card is ", card);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch((error) => {
+  console.log("Error ", error);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
