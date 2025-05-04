ad5c7b7b66907429027908faaa009fa04e6a9323
Denny Wibowo <indocoffee@gmail.com>
2025-05-04 17:22:12 -0400

    Change the initRun to call the functions to insertCards and insertAccounts

--- nodejs/server.js
+++ nodejs/server.js
@@ -229,40 +229,18 @@ async function insertAccounts() {
 
   const result = await account.insertOne(mockAccount);
 }
-async function run() {
+
+async function initRun() {
   try {
-    const database = client.db("ccDb");
-    console.log("Database connected ", database);
-    const cards = database.collection("cards");
-    await cards.deleteMany();
-
-    const visaCard = {
-      name: "Visa",
-      image: "assets/visa.jpg",
-      features: ["3% cash return", "Milage points"],
-      rules: ["$90 annual fee"],
-    };
-
-    const amexCard = {
-      name: "American Express",
-      image: "assets/amex.jpg",
-      features: ["2% cash back", "Free rental insurance"],
-      rules: [],
-    };
-
-    const result = await cards.insertMany([visaCard, amexCard]);
-    console.log(`Document/s inserted: ${result}`);
-
-    const query = { name: "Visa" };
-    const card = await cards.findOne(query);
-    console.log("Card is ", card);
+    await insertCards();
+    await insertAccounts();
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 
-run().catch((error) => {
+initRun().catch((error) => {
   console.log("Error ", error);
 });
 
