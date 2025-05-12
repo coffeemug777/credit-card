908fe1d6e2f906cb0ea9134e261efc981b89fe14
Denny Wibowo <indocoffee@gmail.com>
2025-05-12 08:29:03 -0400

    Add logic to getRecentActivities

--- angular/src/app/services/cc.service.ts
+++ angular/src/app/services/cc.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import mockData from "../../assets/mock.json";
 import { firstValueFrom, of } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+const DB_URL = "http://localhost:3000";
 
 @Injectable({
   providedIn: "root",
@@ -23,17 +24,28 @@ export class CcService {
     return false;
   }
 
-  getRecentActivities() {
-    return false;
+  async getRecentActivities(): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get(
+          `${DB_URL}/transaction/${
+            this.persistentAccount.cards[this.persistentAccount.active_card].id
+          }`
+        )
+      );
+      console.log("Response get recent activities ", response);
+      return response;
+    } catch (error) {
+      console.log("Error ", error);
+      return of([]);
+    }
   }
 
   async login() {
     this.persistentAccount = mockData.account[0];
     try {
       let response = await firstValueFrom(
-        this.http.get<any>(
-          "http://localhost:3000/account?id=67bbb2c799a203195fc2f300"
-        )
+        this.http.get<any>(DB_URL + "/account?id=67bbb2c799a203195fc2f300")
       );
       console.log("response is ", response);
       this.persistentAccount = response;
@@ -48,7 +60,7 @@ export class CcService {
     console.log("transaction ", transaction);
     try {
       const response = await firstValueFrom(
-        this.http.post("http://localhost:3000/transaction", transaction)
+        this.http.post(DB_URL + "/transaction", transaction)
       );
       console.log("response from transaction insert is ", response);
     } catch (error) {
