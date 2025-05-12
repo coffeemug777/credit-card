4c788aa402b7e44f373bff20b93c3dba4930230c
Denny Wibowo <indocoffee@gmail.com>
2025-05-12 08:29:46 -0400

    Refactor how transactions are displayed to conform getting data from service instead of mocks

--- angular/src/app/components/card-recent-activities/card-recent-activities.component.ts
+++ angular/src/app/components/card-recent-activities/card-recent-activities.component.ts
@@ -1,22 +1,40 @@
 import { Component, inject, OnInit } from "@angular/core";
 import { CcService } from "../../services/cc.service";
-import { DatePipe, JsonPipe, KeyValuePipe, NgFor, NgIf } from "@angular/common";
+import {
+  CurrencyPipe,
+  DatePipe,
+  KeyValuePipe,
+  NgFor,
+  NgIf,
+} from "@angular/common";
 
 @Component({
   selector: "app-card-recent-activities",
-  imports: [NgFor, DatePipe, NgIf],
+  imports: [NgFor, DatePipe, NgIf, KeyValuePipe, CurrencyPipe],
   templateUrl: "./card-recent-activities.component.html",
   styleUrl: "./card-recent-activities.component.scss",
 })
 export class CardRecentActivitiesComponent implements OnInit {
   private ccService = inject(CcService);
-  transactions: any;
+  transactions: any[] = [];
+  transactionMap = new Map();
 
-  ngOnInit(): void {
-    const rawTransaction = this.ccService.getActiveCard()["transactions"];
-    this.transactions = rawTransaction.sort((a: any, b: any) => {
-      console.log(a.date, b.date);
-      return new Date(b.date).getTime() - new Date(a.date).getTime();
+  /** ngOnInit
+   * get raw transactions, sort by date and then group them byday
+   */
+  async ngOnInit(): Promise<void> {
+    const rawTransaction: any[] = await this.ccService.getRecentActivities();
+
+    const sortedRawTransaction = rawTransaction.sort(
+      (a: any, b: any) =>
+        new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+
+    sortedRawTransaction.forEach((transaction) => {
+      const dateString = new Date(transaction.date).toISOString().split("T")[0];
+      this.transactionMap.has(dateString)
+        ? this.transactionMap.get(dateString).push(transaction)
+        : this.transactionMap.set(dateString, [transaction]);
     });
   }
 }
