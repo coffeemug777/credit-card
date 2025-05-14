5a29f248b5df26eb46ffadf3d4cc616d4d58b1ca
Denny Wibowo <indocoffee@gmail.com>
2025-05-13 21:21:37 -0400

    Add button and function to insert a bunch of dummy data

--- angular/src/app/components/mock/mock.component.ts
+++ angular/src/app/components/mock/mock.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { faker } from "@faker-js/faker";
 import { CcService } from "../../services/cc.service";
+import { format, sub } from "date-fns";
 
 @Component({
   selector: "app-mock",
@@ -9,8 +10,10 @@ import { CcService } from "../../services/cc.service";
   templateUrl: "./mock.component.html",
   styleUrl: "./mock.component.scss",
 })
-export class MockComponent {
+export class MockComponent implements OnInit {
   ccService = inject(CcService);
+
+  ngOnInit(): void {}
 
   onInsertNewTransaction() {
     const newDate = faker.date.recent();
@@ -31,5 +34,43 @@ export class MockComponent {
     };
 
     this.ccService.insertTransaction(newTransaction);
+  }
+
+  onTwoMonthsGenerateTransactions() {
+    const todayDate = format(new Date(), "yyyy-MM-dd");
+    const twoMonthDate = format(sub(new Date(), { months: 2 }), "yyyy-MM-dd");
+
+    let temp = [];
+    for (let i = 0; i < 200; i++) {
+      const newDate = faker.date.between({
+        from: twoMonthDate,
+        to: todayDate,
+      });
+      const newTransaction = {
+        account_id: "1",
+        description: faker.company.name(),
+        card_id: "1",
+        amount: Math.round(Math.random() * 100 * 100) / 100,
+        type: "debit",
+        date: newDate.toISOString(),
+        date_group:
+          newDate.getFullYear() +
+          "" +
+          (newDate.getMonth() + 1) +
+          "" +
+          newDate.getDate(),
+      };
+
+      temp.push(newTransaction);
+    }
+
+    temp.sort(
+      (a, b) => new Date(a.date).getUTCDate() - new Date(b.date).getUTCDate()
+    );
+    temp.forEach((transaction) => {
+      this.ccService.insertTransaction(transaction);
+    });
+
+    console.log("aaaa ", todayDate, twoMonthDate, temp);
   }
 }
