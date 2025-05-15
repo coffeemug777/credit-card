02ff7e75afa510aa506d7ca08950fccd09950a23
Denny Wibowo <indocoffee@gmail.com>
2025-05-15 18:14:26 -0400

    Refactor login and route so it load user and active card first

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, inject, Input, OnInit } from "@angular/core";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
@@ -16,6 +16,7 @@ import { firstValueFrom } from "rxjs";
     MatInputModule,
     MatButtonModule,
     MatDividerModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   styleUrls: ["./login.component.css"],
@@ -25,14 +26,36 @@ export class LoginComponent implements OnInit {
   private readonly ccService = inject(CcService);
   private readonly router = inject(Router);
 
+  userName = new FormControl("Johndoe");
+  password = new FormControl("password123");
+
   ngOnInit(): void {
     console.log("LoginComponent initialized");
     //TODO: remove debug logic below
-    this.login();
+    //this.login();
   }
 
   login() {
-    this.ccService.login();
-    this.router.navigate(["/dashboard"]);
+    this.ccService
+      .login(this.userName.value || "", this.password.value || "")
+      .subscribe({
+        next: async (response) => {
+          this.ccService.setPersistentAccount(response);
+
+          this.ccService.setActiveCardState(
+            await firstValueFrom(this.ccService.getActiveCard())
+          );
+          console.log(
+            "Login success ",
+            this.ccService.getActiceCardState(),
+            this.ccService.getPersistenAccount()
+          );
+
+          this.router.navigate(["/dashboard"]);
+        },
+        error: (error) => {
+          console.log("Login Error ", error);
+        },
+      });
   }
 }
