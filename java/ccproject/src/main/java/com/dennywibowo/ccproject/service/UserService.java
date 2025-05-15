b8255f54e93d83585b297186300325be045fc659
Denny Wibowo <indocoffee@gmail.com>
2025-05-15 18:11:49 -0400

    Remove Interface Impl pattern, not needed

--- java/ccproject/src/main/java/com/dennywibowo/ccproject/service/UserService.java
+++ java/ccproject/src/main/java/com/dennywibowo/ccproject/service/UserService.java
@@ -1,8 +1,21 @@
 package com.dennywibowo.ccproject.service;
 
 import com.dennywibowo.ccproject.model.User;
+import com.dennywibowo.ccproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    User saveUser(User user);
-    User getUserById(Long id);
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    public User getUserById(Long id) {
+        return userRepository.getReferenceById(id);
+    }
+    public User findByUserNameAndPassword(String userName, String password){return userRepository.findByUserNameAndPassword(userName, password);}
 }
