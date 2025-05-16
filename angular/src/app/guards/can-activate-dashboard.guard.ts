d10623338ca8dd3010cb889c089cb92b41ee62b6
Denny Wibowo <indocoffee@gmail.com>
2025-05-15 20:05:19 -0400

    fix debug code to login when routed to dashboard

--- angular/src/app/guards/can-activate-dashboard.guard.ts
+++ angular/src/app/guards/can-activate-dashboard.guard.ts
@@ -1,11 +1,20 @@
 import { CanActivateFn } from "@angular/router";
 import { CcService } from "../services/cc.service";
 import { inject } from "@angular/core";
+import { firstValueFrom } from "rxjs";
 
-export const canActivateDashboardGuard: CanActivateFn = (route, state) => {
+export const canActivateDashboardGuard: CanActivateFn = async (
+  route,
+  state
+) => {
   const ccService = inject(CcService);
 
   //TODO: probably use localStorage here instead of calling login here.
-  ccService.login();
+  ccService.setPersistentAccount(
+    await firstValueFrom(ccService.login("Johndoe", "password123"))
+  );
+  ccService.setActiveCardState(await firstValueFrom(ccService.getActiveCard()));
+
+  //  console.log("from guard ", result);
   return true;
 };
