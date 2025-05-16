c28fe84ffb723dc17eb0fb413e6bc4e585d4a028
Denny Wibowo <indocoffee@gmail.com>
2025-05-15 21:18:47 -0400

    Add  user model

---
+++ angular/src/app/model/user.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id?: number;
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+  monthlyIncome: number;
+  annualIncome: number;
+  activeCard: number;
+  userName: string;
+  password: string;
+};
