4989d62cf25856610b4982fa1fa1ed9adfd940fa
Denny Wibowo <indocoffee@gmail.com>
2025-06-19 09:36:05 -0400

    Comment out debug lines

--- java/ccproject/src/main/java/com/dennywibowo/ccproject/CcprojectApplication.java
+++ java/ccproject/src/main/java/com/dennywibowo/ccproject/CcprojectApplication.java
@@ -1,13 +1,15 @@
 package com.dennywibowo.ccproject;
 
 import com.dennywibowo.ccproject.model.Card;
+import com.dennywibowo.ccproject.model.User;
 import com.dennywibowo.ccproject.repository.CardRepository;
+import com.dennywibowo.ccproject.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dennywibowo")
 public class CcprojectApplication {
 
 	public static void main(String[] args) {
@@ -15,11 +17,14 @@ public class CcprojectApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(CardRepository cardRepository) {
+	public CommandLineRunner demo(CardRepository cardRepository, UserRepository userRepository) {
 		return (args) -> {
-			cardRepository.deleteAll();
-			cardRepository.save(new Card(null,"Visa","assets/visa.jpg"));
-			cardRepository.save(new Card(null,"American Express","assets/amex.jpg"));
+//			cardRepository.deleteAll();
+//			cardRepository.save(new Card(null,"Visa","assets/visa.jpg"));
+//			cardRepository.save(new Card(null,"American Express","assets/amex.jpg"));
+
+//			userRepository.deleteAll();
+//			userRepository.save(new User(null, "John Doe", "john.doe@example.com", "123 Main st, Anytonw, USA", "123456890", 50000, 4166.67f, 0L, "Johndoe", "password123"));
 		};
 	}
 
